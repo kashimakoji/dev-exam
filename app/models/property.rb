b5f62fcf6095117b82301e99459493648c16da14
atsushi oshima <aboutagial@yahoo.co.jp>
2021-09-11 17:32:02 +0900

    Nearest_stモデル作成

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,4 @@
 class Property < ApplicationRecord
-  has_many :nearest_sts
+  has_many :nearest_sts, dependent: :destroy, inverse_of: :property
+  accepts_nested_attributes_for :nearest_sts, allow_destroy: true, reject_if: :all_blank
 end
